9e1acb88042341f7060ea25e402a6add9ba81f31
HuffPuffTuff <mrsalevan@gmail.com>
2022-11-04 20:11:21 +0400

    feat: complete 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const path = require('path');
+const fs = require('fs');
 
+const dir = path.resolve(__dirname, 'files');
+
+fs.mkdir(
+  path.join(__dirname, 'files-copy'),
+  { recursive: true },
+  (err) => {
+    if (err) return console.error(err);
+
+    console.log('Directory created successfully!');
+  }
+);
+
+
+fs.readdir(
+  dir,
+  (err, files) => {
+    if (err) throw err;
+    
+    files.forEach( file => {
+      const filePath = path.resolve(__dirname, 'files', file);
+      const copyPath = path.resolve(__dirname, 'files-copy', file);
+
+      fs.copyFile(filePath, copyPath, (err) => {
+        if (err) throw err;
+        console.log(`/files/${file} was copied to /files-copy/${file}`);
+      });
+    });
+  }
+)
+// fs.copyFile()
+
+// console.log(dir)
